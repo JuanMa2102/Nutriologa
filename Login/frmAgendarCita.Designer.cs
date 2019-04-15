77552ea6ac4b2c48ff445b8f63be1d39b0d28110
JuanMa2102 <40833994+JuanMa2102@users.noreply.github.com>
2019-04-15 11:41:15 -0500

    JuanMa
    AgendarCita

--- Login/frmAgendarCita.Designer.cs
+++ Login/frmAgendarCita.Designer.cs
@@ -28,101 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelNav = new System.Windows.Forms.Panel();
-            this.btnCerrar = new System.Windows.Forms.Button();
-            this.btnGenerar = new System.Windows.Forms.Button();
-            this.btnLiberar = new System.Windows.Forms.Button();
-            this.btnConsultar = new System.Windows.Forms.Button();
-            this.btnCita = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNuevaCita = new System.Windows.Forms.Button();
             this.btnAtras = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnPaciente = new System.Windows.Forms.Button();
-            this.imageUsuario = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panelNav.SuspendLayout();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageUsuario)).BeginInit();
             this.SuspendLayout();
             // 
-            // panelNav
+            // label1
             // 
-            this.panelNav.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.panelNav.AutoSize = true;
-            this.panelNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
-            this.panelNav.Controls.Add(this.imageUsuario);
-            this.panelNav.Controls.Add(this.btnCerrar);
-            this.panelNav.Controls.Add(this.btnGenerar);
-            this.panelNav.Controls.Add(this.btnLiberar);
-            this.panelNav.Controls.Add(this.btnConsultar);
-            this.panelNav.ForeColor = System.Drawing.Color.Transparent;
-            this.panelNav.Location = new System.Drawing.Point(32, 12);
-            this.panelNav.Name = "panelNav";
-            this.panelNav.Size = new System.Drawing.Size(206, 616);
-            this.panelNav.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
+            this.label1.Location = new System.Drawing.Point(54, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "AGENDAR CITA";
             // 
-            // btnCerrar
+            // btnNuevaCita
             // 
-            this.btnCerrar.Font = new System.Drawing.Font("Arial", 12F);
-            this.btnCerrar.ForeColor = System.Drawing.Color.Black;
-            this.btnCerrar.Location = new System.Drawing.Point(13, 409);
-            this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(180, 48);
-            this.btnCerrar.TabIndex = 6;
-            this.btnCerrar.Text = "Cerrar Sesion";
-            this.btnCerrar.UseVisualStyleBackColor = true;
-        //    this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
-            // 
-            // btnGenerar
-            // 
-            this.btnGenerar.Font = new System.Drawing.Font("Arial", 12F);
-            this.btnGenerar.ForeColor = System.Drawing.Color.Black;
-            this.btnGenerar.Location = new System.Drawing.Point(13, 334);
-            this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(180, 48);
-            this.btnGenerar.TabIndex = 2;
-            this.btnGenerar.Text = "Generar Ficha";
-            this.btnGenerar.UseVisualStyleBackColor = true;
-            // 
-            // btnLiberar
-            // 
-            this.btnLiberar.Font = new System.Drawing.Font("Arial", 12F);
-            this.btnLiberar.ForeColor = System.Drawing.Color.Black;
-            this.btnLiberar.Location = new System.Drawing.Point(13, 260);
-            this.btnLiberar.Name = "btnLiberar";
-            this.btnLiberar.Size = new System.Drawing.Size(180, 48);
-            this.btnLiberar.TabIndex = 1;
-            this.btnLiberar.Text = "Liberar Paciente";
-            this.btnLiberar.UseVisualStyleBackColor = true;
-            // 
-            // btnConsultar
-            // 
-            this.btnConsultar.FlatAppearance.BorderSize = 0;
-            this.btnConsultar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConsultar.ForeColor = System.Drawing.Color.Black;
-            this.btnConsultar.Location = new System.Drawing.Point(13, 187);
-            this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(180, 48);
-            this.btnConsultar.TabIndex = 0;
-            this.btnConsultar.Text = "Consultar Cita";
-            this.btnConsultar.UseVisualStyleBackColor = true;
-            // 
-            // btnCita
-            // 
-            this.btnCita.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnCita.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
-            this.btnCita.FlatAppearance.BorderSize = 0;
-            this.btnCita.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(81)))));
-            this.btnCita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCita.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCita.ForeColor = System.Drawing.Color.White;
-            this.btnCita.Image = global::Login.Properties.Resources.agenda_estilo2;
-            this.btnCita.Location = new System.Drawing.Point(380, 126);
-            this.btnCita.Name = "btnCita";
-            this.btnCita.Size = new System.Drawing.Size(318, 389);
-            this.btnCita.TabIndex = 8;
-            this.btnCita.Text = "AGENDAR CITA";
-            this.btnCita.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnCita.UseVisualStyleBackColor = false;
+            this.btnNuevaCita.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNuevaCita.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
+            this.btnNuevaCita.FlatAppearance.BorderSize = 0;
+            this.btnNuevaCita.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(81)))));
+            this.btnNuevaCita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevaCita.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevaCita.ForeColor = System.Drawing.Color.White;
+            this.btnNuevaCita.Image = global::Login.Properties.Resources.nuevacita_estilo1;
+            this.btnNuevaCita.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnNuevaCita.Location = new System.Drawing.Point(32, 105);
+            this.btnNuevaCita.Name = "btnNuevaCita";
+            this.btnNuevaCita.Size = new System.Drawing.Size(216, 149);
+            this.btnNuevaCita.TabIndex = 8;
+            this.btnNuevaCita.Text = "NUEVA CITA";
+            this.btnNuevaCita.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnNuevaCita.UseVisualStyleBackColor = false;
             // 
             // btnAtras
             // 
@@ -131,7 +75,7 @@
             this.btnAtras.FlatAppearance.BorderSize = 0;
             this.btnAtras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAtras.Image = global::Login.Properties.Resources.flecha_blanca02;
-            this.btnAtras.Location = new System.Drawing.Point(380, 582);
+            this.btnAtras.Location = new System.Drawing.Point(32, 576);
             this.btnAtras.Name = "btnAtras";
             this.btnAtras.Size = new System.Drawing.Size(136, 46);
             this.btnAtras.TabIndex = 7;
@@ -143,93 +87,78 @@
             this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pictureBox1.Image = global::Login.Properties.Resources.header_sistema;
             this.pictureBox1.InitialImage = global::Login.Properties.Resources.WhatsApp_Image_2019_04_07_at_11_22_25_PM;
-            this.pictureBox1.Location = new System.Drawing.Point(380, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(32, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(782, 77);
+            this.pictureBox1.Size = new System.Drawing.Size(1130, 77);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
-            // btnPaciente
+            // btn2
             // 
-            this.btnPaciente.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnPaciente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
-            this.btnPaciente.FlatAppearance.BorderSize = 0;
-            this.btnPaciente.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(81)))));
-            this.btnPaciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPaciente.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPaciente.ForeColor = System.Drawing.Color.Transparent;
-            this.btnPaciente.Image = global::Login.Properties.Resources.pacientes_estilo1;
-            this.btnPaciente.Location = new System.Drawing.Point(844, 126);
-            this.btnPaciente.Name = "btnPaciente";
-            this.btnPaciente.Size = new System.Drawing.Size(318, 389);
-            this.btnPaciente.TabIndex = 5;
-            this.btnPaciente.Text = "PACIENTES";
-            this.btnPaciente.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnPaciente.UseVisualStyleBackColor = false;
-       //     this.btnPaciente.Click += new System.EventHandler(this.btnPaciente_Click);
+            this.btn2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(81)))));
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Image = global::Login.Properties.Resources.nuevacita_estilo1;
+            this.btn2.Location = new System.Drawing.Point(32, 260);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(216, 146);
+            this.btn2.TabIndex = 10;
+            this.btn2.Text = "NUEVA CITA";
+            this.btn2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn2.UseVisualStyleBackColor = false;
             // 
-            // imageUsuario
+            // btn3
             // 
-            this.imageUsuario.Image = global::Login.Properties.Resources.thanos_0;
-            this.imageUsuario.Location = new System.Drawing.Point(13, 39);
-            this.imageUsuario.Name = "imageUsuario";
-            this.imageUsuario.Size = new System.Drawing.Size(180, 111);
-            this.imageUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageUsuario.TabIndex = 7;
-            this.imageUsuario.TabStop = false;
+            this.btn3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(81)))));
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.Image = global::Login.Properties.Resources.nuevacita_estilo1;
+            this.btn3.Location = new System.Drawing.Point(32, 412);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(216, 146);
+            this.btn3.TabIndex = 11;
+            this.btn3.Text = "NUEVA CITA";
+            this.btn3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn3.UseVisualStyleBackColor = false;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(66)))), ((int)(((byte)(101)))));
-            this.label1.Location = new System.Drawing.Point(411, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 29);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "MENU PRINCIPAL";
-            // 
-            // frmPrincipal
+            // frmAgendarCita
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1300, 650);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnCita);
+            this.Controls.Add(this.btnNuevaCita);
             this.Controls.Add(this.btnAtras);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.btnPaciente);
-            this.Controls.Add(this.panelNav);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "frmPrincipal";
+            this.Name = "frmAgendarCita";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " ";
-            //this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmPrincipal_FormClosed);
-            //this.Load += new System.EventHandler(this.frmPrincipal_Load);
-            this.panelNav.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageUsuario)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panelNav;
-        private System.Windows.Forms.Button btnPaciente;
-        private System.Windows.Forms.Button btnCerrar;
-        private System.Windows.Forms.Button btnGenerar;
-        private System.Windows.Forms.Button btnLiberar;
-        private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Button btnAtras;
-        private System.Windows.Forms.PictureBox imageUsuario;
-        private System.Windows.Forms.Button btnCita;
+        private System.Windows.Forms.Button btnNuevaCita;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
     }
 }
