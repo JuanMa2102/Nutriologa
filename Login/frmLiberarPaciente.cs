dc80d4472d412b5dc3d677b5e8255d17241cc887
CesarMoGu <49623952+CesarMoGu@users.noreply.github.com>
2019-04-15 11:31:27 -0500

    César Gabriel
    Liberar paciente agregado

---
+++ Login/frmLiberarPaciente.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class frmLiberarPaciente : Form
+    {
+        public frmLiberarPaciente()
+        {
+            InitializeComponent();
+            redondear();
+            redondear(btnSI);
+            redondear(btnNO);
+        }
+        public void redondear(Button btn)
+        {
+            Rectangle r = new Rectangle(0, 0, btn.Width, btn.Height);
+            GraphicsPath gp = new GraphicsPath();
+            int d = 10;
+            gp.AddArc(r.X, r.Y, d, d, 180, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
+            gp.AddArc(r.X, r.Y + r.Height - d, d, d, 90, 90);
+            btn.Region = new Region(gp);
+        }
+        public void redondear(Panel btn)
+        {
+            Rectangle r = new Rectangle(0, 0, btn.Width, btn.Height);
+            GraphicsPath gp = new GraphicsPath();
+            int d = 30;
+            gp.AddArc(r.X, r.Y, d, d, 180, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
+            gp.AddArc(r.X, r.Y + r.Height - d, d, d, 90, 90);
+            btn.Region = new Region(gp);
+        }
+        public void redondear()
+        {
+            Rectangle r = new Rectangle(0, 0, this.Width, this.Height);
+            GraphicsPath gp = new GraphicsPath();
+            int d = 30;
+            gp.AddArc(r.X, r.Y, d, d, 180, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
+            gp.AddArc(r.X, r.Y + r.Height - d, d, d, 90, 90);
+            this.Region = new Region(gp);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
